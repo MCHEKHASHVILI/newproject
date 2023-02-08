c465b603f5d48976e4105e706003895387fc8c2d
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-02-08 16:31:30 +0400

    removed Beta.vue file

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 
 import App from './App.vue'
-// import Beta from './Beta.vue'
 import store from '../store'
-// import store from '../mariami'
 
 createApp(App)
     .use(store)
