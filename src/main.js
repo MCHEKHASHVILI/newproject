7d1ae081089b0dd91f10537bb5d5b94fbbef4cc1
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-02-08 18:08:06 +0400

    added vue router RouterView and RouterLinks to App.vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,43 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 
+
 import App from './App.vue'
 import store from '../store'
 
+/** Vue Router */
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Contact from './views/Contact.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: "MTAVARI",
+        component: Home
+    },
+    {
+        path: '/about',
+        name: "About",
+        component: About
+    },
+    {
+        path: '/contact',
+        name: "Contact",
+        component: Contact
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: routes
+})
+
+// Vue Router
+
 createApp(App)
     .use(store)
+    .use(router)
     .mount('#app')
